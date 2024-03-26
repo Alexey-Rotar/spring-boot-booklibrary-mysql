5060faa75874cc59b287c9fa9394255fe5a38829
alexey.rotar <ar_mail@inbox.ru>
2024-03-26 07:28:19 +0500

    deleted unnecessary

--- src/main/java/ru/gb/springbootlesson3/repository/JpaIssueRepository.java
+++ src/main/java/ru/gb/springbootlesson3/repository/JpaIssueRepository.java
@@ -1,7 +1,6 @@
 package ru.gb.springbootlesson3.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import ru.gb.springbootlesson3.entity.Book;
 import ru.gb.springbootlesson3.entity.Issue;
 
 import java.util.List;
